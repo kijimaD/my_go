a069ec370626ead018386d58133e5b35fb80f51f
kijimaD <norimaking777@gmail.com>
2022-09-16 10:54:04 +0900

    feat: project filter

--- docer_ls/main.go
+++ docer_ls/main.go
@@ -6,11 +6,16 @@ import (
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types"
-	_ "github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
 
 func main() {
+	all()
+	project_filter()
+}
+
+func all() {
 	ctx := context.Background()
 	cli, err := client.NewEnvClient()
 
@@ -22,5 +27,26 @@ func main() {
 	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{
 		All: true,
 	})
-	fmt.Println(containers)
+	fmt.Println("all: ", containers)
+}
+
+func project_filter() {
+	// フィルタ条件を追加する
+	// プロジェクト名testを指定してコンテナを起動しておく
+	// docker-compose -p test up -d
+
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+
+	if err != nil {
+		panic(err)
+	}
+
+	labelFilters := filters.NewArgs()
+	labelFilters.Add("label", "com.docker.compose.project=test")
+	containers_filtered, err := cli.ContainerList(ctx, types.ContainerListOptions{
+		All: true,
+		Filters: labelFilters,
+	})
+	fmt.Println("project filter: ", containers_filtered)
 }
