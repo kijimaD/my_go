a2d74e03ad15748a6118bb7e8017ddc0866978c6
kijimaD <norimaking777@gmail.com>
2022-11-20 21:16:08 +0900

    feat: sumAll

---
+++ test_driven/sum/sum_test.go
@@ -0,0 +1,46 @@
+package sum
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func ExampleSum() {
+	sum := Sum([]int{1, 1, 1, 1, 1})
+	fmt.Println(sum)
+	// Output: 5
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
