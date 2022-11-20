3a75877e10b6a8a90e7e45a6d6b0881baf17b49b
kijimaD <norimaking777@gmail.com>
2022-11-20 23:53:22 +0900

    feat: table driven test

--- test_driven/shapes/shapes_test.go
+++ test_driven/shapes/shapes_test.go
@@ -14,22 +14,25 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+// shapeはinterfaceで、異なる実装を入れられる。これによってテストしやすくなる
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name    string // テストケースの名前として使用する
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0}, // フィールド名をつけるとわかりやすい
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		// t.Runで名前をつけることで、失敗したときにどこで失敗しているのかわかりやすい
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("got %g want %g", got, tt.hasArea)
+			}
+		})
+	}
 }
