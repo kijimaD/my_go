3363fb66a1d7bc84d49203471803dfb117e6a7d3
kijimaD <norimaking777@gmail.com>
2022-11-27 21:31:33 +0900

    feat: array, map

--- test_driven/reflect/reflect_test.go
+++ test_driven/reflect/reflect_test.go
@@ -50,6 +50,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	for _, test := range cases {
