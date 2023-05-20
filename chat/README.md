c124f18b758f57972ebc41b893fa4efde864ac76
kijimaD <norimaking777@gmail.com>
2023-05-20 15:10:04 +0900

    feat: add log handler

--- chat/README.md
+++ chat/README.md
@@ -1,1 +1,4 @@
 『Go言語によるWebアプリケーション開発』のメモ
+
+export GOOGLE_SECRETKEY=xxx
+export GOOGLE_CLIENTID=xxx
--- chat/chat/main.go
+++ chat/chat/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"example/chat/trace"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
 )
+
+type Env struct {
+	GOOGLE_SECRETKEY string `envconfig:"GOOGLE_SECRETKEY" default:""`
+	GOOGLE_CLIENTID  string `envconfig:"GOOGLE_CLIENTID" default:""`
+}
 
 type templateHandler struct {
 	once     sync.Once
@@ -25,11 +35,26 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var env Env
+	err := envconfig.Process("", &env)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't parse environment variables: %s\n", err.Error())
+		os.Exit(1)
+	}
+
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+
+	gomniauth.SetSecurityKey(env.GOOGLE_SECRETKEY)
+	gomniauth.WithProviders(
+		google.New("クライアントID", env.GOOGLE_CLIENTID, "http://localhost:8080/auth/callback/google"),
+	)
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 	log.Println("Webサーバを開始します。ポート:", *addr)
