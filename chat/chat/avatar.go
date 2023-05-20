906cc695ed16a568b722663e9817d2dd9035d417
kijimaD <norimaking777@gmail.com>
2023-05-21 08:49:16 +0900

    feat: implement avatar

---
+++ chat/chat/avatar.go
@@ -0,0 +1,24 @@
+package main
+
+import "errors"
+
+// AvatarインスタンスがアバターのURLを返すことができない場合に発生するエラー
+var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できない")
+
+// ユーザのプロフィール画像を表す型
+type Avatar interface {
+	GetAvatarURL(c *client) (string error)
+}
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
